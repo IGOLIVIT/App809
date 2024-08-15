a052bb6c297df93e6e6953d31ea41c8b6cc90300
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-08-15 11:10:34 +0300

    Initial Commit

---
+++ App809/App809App.swift
@@ -0,0 +1,17 @@
+//
+//  App809App.swift
+//  App809
+//
+//  Created by IGOR on 15/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct App809App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
